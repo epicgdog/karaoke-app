422afea2570769ffcb822db99acfaf21782fd1aa
epicgdog <theepicgdog@gmail.com>
2023-11-08 20:40:53 +0000

    setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+export default function Home() {
+  return (
+    <main>
+
+      <h1 className="text-center"> Title </h1>
+      <input type="text"/>
+      <button> Submit</button>
+      
+    </main>
+  )
+}
